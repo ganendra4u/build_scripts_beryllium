b2d6b63e02717b21297e739606aa534c21b41a18
Ｇａｎｘ <daffaofficial224@gmail.com>
2026-07-07 13:26:40 +0700

    Add build script for Axion AOSP Beryllium
    This script initializes the repo for building the Axion AOSP for the Beryllium device, syncs local manifests, sets up the build environment, and copies output images to a designated folder.

---
+++ build_axion_beryllium.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs --depth=1
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/ganendra4u/local_manifest_beryllium.git .repo/local_manifests -b axion-23.2
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+
+# Build Sync
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=ganx0000
+export BUILD_HOSTNAME=crave
+export AXION_MAINTAINER=ganx0000
+export AXION_BUILD_TYPE=UNOFFICIAL
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+axion beryllium userdebug va
+
+# Build
+ax -br
+
+# Copy imgs to a separate folder for easy download
+mkdir -p imgs_output
+cp out/target/product/beryllium/boot.img imgs_output/
+cp out/target/product/beryllium/dtbo.img imgs_output/
+cp out/target/product/beryllium/recovery.img imgs_output/ 2>/dev/null || true
+cp out/target/product/beryllium/*.zip imgs_output/
